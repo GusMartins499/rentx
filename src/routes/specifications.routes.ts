36a5f01116eafbc54fd16ba314ad6fa019e38cd8
gusmartins499 <gustavosm994@gmail.com>
2021-11-16 18:20:44 +0000

    refactoring specification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,21 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const speficationsRouter = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 speficationsRouter.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  createSpecificationService.excute({ name, description });
-
-  return response.status(201).send();
+  return createSpecificationController.handle(request, response);
 });
 
 export { speficationsRouter };
