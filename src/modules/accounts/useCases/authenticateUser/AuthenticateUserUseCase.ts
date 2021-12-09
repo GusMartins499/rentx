79fe3714c3f7d478a6f20d997303beda77403423
gusmartins499 <gustavosm994@gmail.com>
2021-12-09 13:20:07 +0000

    app error

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../erros/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -28,12 +29,12 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("Email or password incorrect");
+      throw new AppError("Email or password incorrect");
     }
     const passwordMath = await compare(password, user.password);
 
     if (!passwordMath) {
-      throw new Error("Email or password incorrect");
+      throw new AppError("Email or password incorrect");
     }
 
     const token = sign({}, "a99e567044ffd6c21c060bf22abda82e", {
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import "./database";
 import "./shared/container";
 
-import express from "express";
+import express, { Request, NextFunction, Response } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import { AppError } from "./erros/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -14,5 +16,19 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Internal Server Error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(3333, () => console.log("Server is running"));
