a1a925b141d4d623d082bde3f2bd810ecb5430b6
gusmartins499 <gustavosm994@gmail.com>
2021-12-09 12:10:22 +0000

    auth user

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,56 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect");
+    }
+    const passwordMath = await compare(password, user.password);
+
+    if (!passwordMath) {
+      throw new Error("Email or password incorrect");
+    }
+
+    const token = sign({}, "a99e567044ffd6c21c060bf22abda82e", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
